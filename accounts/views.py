d8c877d3940e5e06f64eadf6eb90039ae878a607
danial sadri <danielsadri01@gmail.com>
2023-10-01 15:54:08 +0330

    add register

--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,14 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import UserSerializer
+
+
+class UserRegisterView(APIView):
+    def post(self, request: Request):
+        user_serializer = UserSerializer(data=request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(data={'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
+        return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
