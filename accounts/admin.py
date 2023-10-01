218f2385229207f652d4b3928ab7353c8a3e44de
danial sadri <danielsadri01@gmail.com>
2023-10-01 13:45:00 +0330

    add user

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,1 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ['username', 'email', 'phone_number', 'is_active', 'is_staff', 'is_superuser']
+    list_filter = ['is_active', 'is_staff', 'is_superuser']
+    search_fields = ['username', 'email', 'phone_number']
+
+    fieldsets = [
+        ('Info', {'fields': ['username', 'password']}),
+        ('Personal info', {'fields': ['first_name', 'last_name', 'email']}),
+        ('Permissions', {'fields': ['is_active', 'is_staff', 'is_superuser']}),
+        ('Additional information', {'fields': ['phone_number', 'biography', 'photo']}),
+    ]
--- accounts/models.py
+++ accounts/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from datetime import datetime
+
+
+def get_user_photo(instance, filename):
+    return f"user_photos/{instance.username}/{datetime.now().strftime('%Y/%m/%d')}/{filename}"
+
+
+class User(AbstractUser):
+    phone_number = models.CharField(max_length=11, blank=True, null=True)
+    biography = models.TextField(max_length=500, blank=True, null=True)
+    photo = models.ImageField(upload_to=get_user_photo, blank=True, null=True)
